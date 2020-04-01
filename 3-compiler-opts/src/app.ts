253bbcfffbe7f98f7926904df827e7e9f92de04a
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-04-01 11:54:18 -0500

    configure the output directory to be /dist
    Also configure the sources directory in module 3 as 3-compiler-opts/src

---
+++ 3-compiler-opts/src/app.ts
@@ -0,0 +1,13 @@
+console.log("Module 3-Compiler options");
+
+console.log('"sourceMap": true');
+console.log("outDir to ../dist");
+
+function add(n1:number, n2:number):number{
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result= add(number1, number2);
